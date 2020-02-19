72c38ad92977c079809a0089e3214a414759f5ab
O-Luhishi <oeluhishi@gmail.com>
2020-02-19 15:53:53 +0000

    Added functionality for the user to add books to the database

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="col-sm-10">
         <h1>Books</h1>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Book</button>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
         <br><br>
         <table class="table table-hover">
           <thead>
@@ -34,8 +34,45 @@
         </table>
       </div>
     </div>
+    <b-modal ref="addBookModal"
+            id="book-modal"
+            title="Add a new book"
+            hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addBookForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group"
+                      label="Author:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addBookForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 
@@ -43,6 +80,11 @@ export default {
   data() {
     return {
       books: [],
+      addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   methods: {
@@ -51,12 +93,46 @@ export default {
       axios.get(path)
         .then((res) => {
           this.books = res.data.Books;
-          console.log(res.data.Books);
         })
         .catch((error) => {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    addBook(payload) {
+      const path = 'http://localhost:5000';
+      axios.post(path, payload)
+        .then(() => {
+          this.getBooks();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getBooks();
+        });
+    },
+    initForm() {
+      this.addBookForm.title = '';
+      this.addBookForm.author = '';
+      this.addBookForm.read = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      let read = false;
+      if (this.addBookForm.read[0]) read = true;
+      const payload = {
+        title: this.addBookForm.title,
+        author: this.addBookForm.author,
+        read, // property shorthand
+      };
+      this.addBook(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      this.initForm();
     },
   },
   created() {
